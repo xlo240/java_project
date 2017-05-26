31b53988863e8cb1686fa240ba1a4b8e20b99847
Xlo240 <xlo@inbox.ru>
2017-05-26 23:58:04 +0300

    update

--- Calculate.java
+++ Calculate.java
@@ -1,5 +1,9 @@
 public class Calculate {
 	public static void main(String[] arg) {
-		System.out.println("Hallo World");
+		
+		int first = Integer.valueOf(arg[0]);
+		int second = Integer.valueOf(arg[1]);
+		int summ = first + second;
+		System.out.println("summ = "+ summ);
 	}
 }
